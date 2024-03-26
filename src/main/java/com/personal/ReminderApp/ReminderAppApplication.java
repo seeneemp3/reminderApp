51d90e6bcc829119c7be9fe87d3b8a0576e756d5
seeneemp3 <seeneemp3@gmail.com>
2024-03-26 16:03:31 +0400

    init:

---
+++ src/main/java/com/personal/ReminderApp/ReminderAppApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ReminderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/ReminderApp/ReminderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.ReminderApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
